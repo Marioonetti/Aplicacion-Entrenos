6e836e59e3a7d2225563a90b0e7f2fab1e746772
Mario Onetti <mario.onetti@worldline.com>
2022-05-17 12:54:48 +0200

    ApiError Parseado

--- app/src/main/java/com/example/aplicacionentrenos/data/sources/remote/utils/BaseApiResponse.kt
+++ app/src/main/java/com/example/aplicacionentrenos/data/sources/remote/utils/BaseApiResponse.kt
@@ -1,5 +1,7 @@
 package com.example.aplicacionentrenos.data.sources.remote.utils
 
+import com.example.aplicacionentrenos.domain.model.dto.ApiError
+import com.google.gson.Gson
 import retrofit2.Response
 
 
@@ -25,18 +27,25 @@ abstract class BaseApiResponse {
     }
 
     suspend fun <T> safeApiCall(apiCall: suspend () -> Response<T>): NetworkResult<T> {
+        var result : NetworkResult<T> = NetworkResult.Empty()
         try {
             val response = apiCall()
             if (response.isSuccessful) {
                 val body = response.body()
                 body?.let {
-                    return NetworkResult.Success(body)
+                    result =  NetworkResult.Success(body)
                 }
             }
-            return error("${response.code()} ${response.message()}")
+            else{
+                response.errorBody()?.let { errorBody ->
+                    val apiError : ApiError = Gson().fromJson(errorBody.string(), ApiError::class.java)
+                    result = NetworkResult.Error(apiError.mensaje)
+                }
+            }
         } catch (e: Exception) {
-            return error(e.message ?: e.toString())
+            result = NetworkResult.Error(e.message ?: e.toString())
         }
+        return result
     }
 
     private fun <T> error(errorMessage: String): NetworkResult<T> =
